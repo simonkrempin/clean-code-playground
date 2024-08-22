4e50d4ec9281b2c24e6816db803c4bcfe844e164
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-08-22 20:17:02 +0200

    getter refactoring

--- house.js
+++ house.js
@@ -10,26 +10,34 @@ const phrases = [
     " the rat that ate",
     " the mal that lay in",
     " the house that Jack built",
-]
+];
 
 const args = get_process_args(phrases.length);
 
-console.log(`This is${echo(shuffle(phrases, args.shuffle), args.echo).slice(0, args.print_count).join("")}.`);
+print_house(get_formatter(args.echo), get_orderer(args.shuffle));
 
-function echo(to_echo, echo = false) {
-    if (echo) {
-        return to_echo;
-    }
+function print_house(formatter, orderer) {
+    const ordered = orderer(phrases);
+    const formatted = formatter(ordered);
+    const rangeToPrint = formatted.slice(0, args.print_count);
 
-    return to_echo.map((value) => value.repeat(2));
+    console.log(`This is${rangeToPrint.join("")}.`);
 }
 
-function shuffle(to_shuffle, shuffle = false) {
-    if (!shuffle) {
-        return to_shuffle;
+function get_formatter(echo = false) {
+    if (echo) {
+        return (to_echo) => to_echo;
     }
 
-    return to_shuffle
+    return (to_echo) => to_echo.map((value) => value.repeat(2));
+}
+
+function get_orderer(shuffle = false) {
+    if (!shuffle) {
+        return (to_shuffle) => to_shuffle;
+    }
+
+    return (to_shuffle) => to_shuffle
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
@@ -41,12 +49,10 @@ function get_process_args(default_print_count) {
         .map((value) => value.split("="));
 
     const userInput = Object.fromEntries(args.map(([key, value]) => [
-        key.trim().replace("--", ""),
-        value.trim(),
+        key.trim().replace("--", ""), value.trim(),
     ]));
 
     return {
-        ...userInput,
-        print_count: userInput.print_count ?? default_print_count,
+        ...userInput, print_count: userInput.print_count ?? default_print_count,
     }
 }
